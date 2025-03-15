e98b0bb4b23e09ece0ffcccf6ad55b6b3501e05c
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2025-03-15 17:02:01 +0200

    Implement Getting Current Tab Url and Add The Utils Folder With GetCurrentTabUrl File to Be a Global Utils

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,28 +1,22 @@
-import { useState } from "react"
-import 'style.css'
-import Header from "~Sections/Header"
+import "style.css"
 import FactCard from "~Component/FactCard"
-import Body from "~Sections/Body"
+import Header from "~Sections/Header"
+import GetCurrentTabUrl from "~Utils/GetCurrentTabUrl"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const CurrentUrl = GetCurrentTabUrl();
 
   return (
-      <div className={"w-[400px] h-[600px] flex flex-col" +
-        "items-center justify-center overflow-y-auto flex-grow"}>
-        <Header/>
-        {/*<Body>*/}
-        {/*overflow-y: auto;*/}
-        {/*max-height: 540px;*/}
-        <div className={'absolute top-[60px] overflow-y-auto max-h-[540px] '}>
-        {/*THe Two Components below are above Each Others*/}
-          <FactCard TrueFact={true}/>
-          <FactCard TrueFact={false}/>
-        </div>
-        {/*</Body>*/}
-
-
+    <div
+      className={
+        "w-[400px] h-[600px] flex flex-col" +
+        "items-center justify-center overflow-y-auto flex-grow"
+      }>
+      <Header />
+      <div className={"absolute top-[60px] overflow-y-auto max-h-[540px] "}>
+        <FactCard TrueFact={true} />
       </div>
+    </div>
   )
 }
 
