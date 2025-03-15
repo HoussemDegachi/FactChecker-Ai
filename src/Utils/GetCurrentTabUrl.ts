1221a5e2fb1474fd7583185fdd622d3b131cc6ff
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2025-03-15 17:02:01 +0200

    Implement Getting Current Tab Url and Add The Utils Folder With GetCurrentTabUrl File to Be a Global Utils

---
+++ src/Utils/GetCurrentTabUrl.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const GetCurrentTabUrl = () => {
+  const [CurrentUrl, setCurrentUrl] = useState<string>('');
+
+  const getCurrentTabUrl = async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    setCurrentUrl(tab.url);
+  };
+
+  useEffect(() => {
+    getCurrentTabUrl();
+  }, []);
+
+  useEffect(() => {
+    console.log(CurrentUrl);
+  }, [CurrentUrl]);
+
+  return CurrentUrl;
+};
+
+export default GetCurrentTabUrl;
