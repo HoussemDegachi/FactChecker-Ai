7a43bc63177c81ffb1bcdf128948b76201649e4e
HoussemDegachi <houssemdegach@gmail.com>
2025-03-17 07:43:37 +0100

    Added AnalysisContext

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import {Route, Routes} from 'react-router-dom'
-
 import Dashboard from './pages/Dashboard'
 import './styles/style.css'
-
-
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
 
   return (
     <>
     <Routes>
-      <Route path='/dashboard/:ytid' element={< Dashboard />} /> 
+      <Route path='/analysis/:ytid' element={< Dashboard />} /> 
+      <Route path='*' element={<NotFoundPage />} />
     </Routes>
     </>
   )
