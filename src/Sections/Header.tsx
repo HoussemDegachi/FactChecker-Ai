9993ab1dca33b3d49e431574eaf298dc5c728b6b
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2025-03-19 22:00:35 +0200

    Adding Our Color Set in Content.ts and Add Transform Translate to The Bubbles to be more accurate and Adding THe Glow to The Text Logo

--- src/Sections/Header.tsx
+++ src/Sections/Header.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 
 const Header = ({isAnalyzing}) => {
+  const TextLogoShadow = {
+    textShadow:"0px 0px 10px cyan," +
+      "0px 0px 20px cyan," +
+      "0px 0px 40px cyan," +
+      "0px 0px 80px cyan;"
+  }
   return (
     // bg-[#4F46E5]
     <nav
@@ -9,7 +15,7 @@ const Header = ({isAnalyzing}) => {
         " px-4 py-2 text-base rounded-b-lg sticky top-0 left-0"
       }>
       <div className={"flex items-center justify-center"}>
-        <h3 className={"text-white text-2xl font-bold"}>FactChecker</h3>
+        <h3 className={"text-white text-2xl font-bold TextLogoShadow"}>FactChecker-Ai</h3>
       </div>
       {/*<div className={"flex items-center justify-between w-[25%]"}>*/}
       {/*  <div className={`w-[10px] h-[10px] bg-[#10B981] rounded-full ${isAnalyzing ? 'animate-pulse' : ''}`}></div>*/}
